c32a85435d75ede7074529ae8a885e3cfb28c036
sjcowdery96 <sjc.cowdery@gmail.com>
2024-04-01 21:20:53 -0500

    added more UI

---
+++ app/components/ScoreBoard.tsx
@@ -0,0 +1,92 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { clientContext } from '../contexts/ClientMoveContext'
+
+interface GameDataDisplay {
+    P1Score: number,
+    P2Score: number,
+    P1SeedBank: number,
+    P2SeedBank: number,
+    //supply
+    P1SeedSupply: number,
+    P2SeedSupply: number,
+    P1DesertSupply: number,
+    P2DesertSupply: number,
+
+}
+const ScoreBoard = () => {
+    //initialize scoreboard
+    const [myGameData, setMyGameData] = useState<GameDataDisplay>({
+        P1Score: 0,
+        P2Score: 0,
+        P1SeedBank: 0,
+        P2SeedBank: 0,
+        //supply
+        P1SeedSupply: 36,
+        P2SeedSupply: 36,
+        P1DesertSupply: 4,
+        P2DesertSupply: 4,
+    })
+    //use context refresher to trigger re-render of the board
+    const { Refresher } = useContext(clientContext)
+    //method for fetching boardData
+    const fetchData = async () => {
+        const response = await fetch('http://localhost:3000/api/process-move');
+        //extract scoreboard data and seed stack data from the response
+        const { gameData } = await response.json()
+        //assign variables 
+        setMyGameData({
+            P1DesertSupply: gameData.P1DesertStack.length,
+            P2DesertSupply: gameData.P2DesertStack.length,
+            P1SeedSupply: gameData.P1SeedStack.length,
+            P2SeedSupply: gameData.P2SeedStack.length,
+            P1Score: gameData.Scoreboard[0],
+            P2Score: gameData.Scoreboard[1],
+            P1SeedBank: gameData.Scoreboard[2],
+            P2SeedBank: gameData.Scoreboard[3],
+        })
+    }
+    //runs twice
+    useEffect(() => {
+        fetchData()
+    }, [Refresher])
+
+    return (
+        <div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Score</th>
+                            <th>Seed Bank</th>
+                            <th>Seeds</th>
+                            <th>Deserts</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr className="hover">
+                            <th>Player 1</th>
+                            <td>{myGameData.P1Score}</td>
+                            <td>{myGameData.P1SeedBank}</td>
+                            <td>{myGameData.P1SeedSupply}</td>
+                            <td>{myGameData.P1DesertSupply}</td>
+                        </tr>
+                        {/* row 2 */}
+                        <tr className="hover">
+                            <th>Player 2</th>
+                            <td>{myGameData.P2Score}</td>
+                            <td>{myGameData.P2SeedBank}</td>
+                            <td>{myGameData.P2SeedSupply}</td>
+                            <td>{myGameData.P2DesertSupply}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+
+}
+
+export default ScoreBoard
---
+++ app/components/UserMoveInput.tsx
@@ -0,0 +1,96 @@
+'use client'
+import React, { useContext, useEffect, useState } from 'react'
+import { clientContext } from '../contexts/ClientMoveContext';
+
+interface Move {
+    MoveType: string; //options: move pieces, place piece seed, place piece desert, place piece koya
+    Player: number; //options: 1 or 2
+    TargetSpaceID: number; //options between 0 to (gameboard width)^2
+}
+
+const UserMoveInput = () => {
+    //useState for string or null
+    const [selectedValue, setSelectedValue] = useState<string>("seed");
+    const [selectedPlayer, setSelectedPlayer] = useState<string>('');
+
+    //using Context
+    const { ClientMove, setClientMove, setRefresher } = useContext(clientContext)
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedValue(event.target.value);
+        setClientMove({ MoveType: event.target.value, Player: ClientMove.Player, TargetSpaceID: ClientMove.TargetSpaceID })
+    };
+    const handleChange3 = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedPlayer(event.target.value);
+        setClientMove({ MoveType: ClientMove.MoveType, Player: parseInt(event.target.value), TargetSpaceID: ClientMove.TargetSpaceID })
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        //prevents silly things
+        event.preventDefault();
+        //matches the inputs to the correct format for the move API
+        const newMove = {
+            MoveType: selectedValue,
+            Player: parseInt(selectedPlayer),//need to convert to int
+            TargetSpaceID: ClientMove.TargetSpaceID
+        }
+        //only updates relevant data feilds
+        setClientMove({ MoveType: newMove.MoveType, Player: newMove.Player, TargetSpaceID: ClientMove.TargetSpaceID })
+        event.preventDefault();
+        //send move to the API (MOVE TO ClientDisplay component?)
+        const response = await fetch('http://localhost:3000/api/process-move', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newMove),
+        });
+        if (!response.ok) {
+            //bad response
+        }
+        else {
+            // Handle successful response --> hit the refresher
+            setRefresher((Math.random() * 50000))
+        }
+
+    };
+    return (
+        <div>
+            <div id='move-display' className="flex flex-row justify-center gap-20">
+                <form onSubmit={handleSubmit} className="flex align-middle flex-row gap-2">
+                    <div>
+                        <span className="form-control">
+                            <label className="label cursor-pointer">
+                                <span className="label-text">Desert 🪨</span>
+                                <input onChange={handleChange} type="radio" name="radio-10" value="D" className="radio checked:bg-orange-900" checked={selectedValue === 'D'} />
+                            </label>
+                        </span>
+                        <span className="form-control">
+                            <label className="label cursor-pointer">
+                                <span className="label-text">Seed 🌱</span>
+                                <input onChange={handleChange} type="radio" name="radio-10" value="S" className="radio checked:bg-green-900" checked={selectedValue === 'S'} />
+                            </label>
+                        </span>
+                    </div>
+                    <div>
+                        <span className="form-control">
+                            <label className="label cursor-pointer">
+                                <span className="label-text">Player 1 🌳</span>
+                                <input onChange={handleChange3} type="radio" name="radio-11" value={1} className="radio checked:bg-orange-300" checked={selectedPlayer === '1'} />
+                            </label>
+                        </span>
+                        <span className="form-control">
+                            <label className="label cursor-pointer">
+                                <span className="label-text">Player 2 🌲</span>
+                                <input onChange={handleChange3} type="radio" name="radio-11" value={0} className="radio checked:bg-green-500" checked={selectedPlayer === '0'} />
+                            </label>
+                        </span>
+                    </div>
+                    <div className='align-middle'>
+                        <button className='btn btn-primary btn-lg' type='submit'>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default UserMoveInput
